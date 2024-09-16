276e7410091a650131c090c7a6fb43d5fcb5f2dc
Lucas Zamblera <lucaszamblers@gmail.com>
2024-09-16 10:13:31 -0300

    actualizacion efecto texto

--- src/components/TypingText.js
+++ src/components/TypingText.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 const TypingText = () => {
   const [text, setText] = useState('');
   const [index, setIndex] = useState(0);
+  const [blink, setBlink] = useState(true); // Define el estado 'blink'
   const message = 'GEA LA PREPAGA QUE NECESITAS';
   const speed = 170; // Velocidad de escritura en milisegundos
 
@@ -22,20 +23,30 @@ const TypingText = () => {
     }
   }, [index, message, speed]);
 
+  // Cursor parpadeante
+  useEffect(() => {
+    const blinkInterval = setInterval(() => {
+      setBlink((prev) => !prev);
+    }, 500);
+    return () => clearInterval(blinkInterval);
+  }, []);
+
   return (
     <div style={typingStyle}>
       {text}
+      <span style={{ opacity: blink ? 1 : 0 }}>|</span> {/* Cursor parpadeante */}
     </div>
   );
 };
 
 const typingStyle = {
-  fontSize: '22px',
+  fontSize: '21px',
   color: 'white',
   fontWeight: 'bold',
   textAlign: 'center',
   padding: '15px',
   textShadow: '6px 6px 8px rgba(0, 0, 0, 0.8)',
+  fontFamily: 'monospace', // Agrega una fuente tipo "teletipo"
 };
 
 export default TypingText;
